abbd19370e1242756b810bfdbcc708f207eb105f
ElusiveToaster <asgloki10@gmail.com>
2023-08-28 18:47:34 +0100

    Two players now take turns

--- script.js
+++ script.js
@@ -1,60 +1,75 @@
 const gameBoard = (() => {
     tile = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    row1 = [0, 0];
-    row2 = [0, 0];
-    row3 = [0, 0];
-    column1 = [0, 0];
-    column2 = [0, 0];
-    column3 = [0, 0];
+    
+    const nextTurn = () => {
+        
 
-    return {tile, row1, row2, row3, column1, column2, column3};
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+        console.log(currentPlayer);
+    }
+
+
+    return {tile, nextTurn};
 })();
 
+
+
 const player = (title, side) => {
+
+    let rows = [0, 0, 0];
+
+    let columns = [0, 0, 0];
+
    
     const placeTile = (tile) => {
         const tileDiv = document.getElementById(tile.target.id);
         const playerToken = document.createElement('h1');
         
             if (tile.target.id == 1 || tile.target.id == 2 || tile.target.id == 3) {
-                gameBoard.row1[0]++;
-                //console.log(gameBoard.row1[0])
+                currentPlayer.rows[0]++;
+                //console.log(row1);
             }
             else if (tile.target.id == 4 || tile.target.id == 5 || tile.target.id == 6) {
-                gameBoard.row2[0]++;
+                currentPlayer.rows[1]++;
                 //console.log(gameBoard.row2[0])
             }
             else if (tile.target.id == 7 || tile.target.id == 8 || tile.target.id == 9) {
-                gameBoard.row3[0]++;
+                currentPlayer.rows[2]++;
                 //console.log(gameBoard.row3[0])
             }
 
             if (tile.target.id == 1 || tile.target.id == 4 || tile.target.id == 7) {
-                gameBoard.column1[0]++;
+                currentPlayer.columns[0]++;
                 //console.log(gameBoard.column1[0])
             }
             else if (tile.target.id == 2 || tile.target.id == 5 || tile.target.id == 8) {
-                gameBoard.column2[0]++;
+                currentPlayer.columns[1]++;
                 //console.log(gameBoard.column2[0])
             }
             else if (tile.target.id == 3 || tile.target.id == 6 || tile.target.id == 9) {
-                gameBoard.column3[0]++;
+                currentPlayer.columns[2]++;
                 //console.log(gameBoard.column3[0])
             }
 
             // Increases score for each row and column
 
-        playerToken.innerText = side;
+        playerToken.innerText = currentPlayer.side;
         tileDiv.appendChild(playerToken);
+        gameBoard.nextTurn();
     };
 
 
-    return {title, side, placeTile};
+    return {title, side, placeTile, rows, columns};
 
 };
 
 const player1 = player(1, "X");
 const player2 = player(2, "O");
+let currentPlayer = player1;
 
 console.log(gameBoard.tile);
 
@@ -65,7 +80,7 @@ gameBoard.tile.forEach((tileNumber) => {
     tileDiv.classList.add('tile');
     tileDiv.setAttribute('id', tileNumber);
 
-    tileDiv.addEventListener('click', player1.placeTile);
+    tileDiv.addEventListener('click', currentPlayer.placeTile);
     
     gameboard.appendChild(tileDiv);
 });
